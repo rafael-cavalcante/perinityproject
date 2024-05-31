ee7ff6c73f6c2cac475b2198d7205ab13d80d4d5
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2024-05-31 15:01:33 -0300

    Create TarefaRepository.java

---
+++ src/main/java/com/rafaelcavalcante/perinityproject/repository/TarefaRepository.java
@@ -0,0 +1,9 @@
+package com.rafaelcavalcante.perinityproject.repository;
+
+import com.rafaelcavalcante.perinityproject.model.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+}
