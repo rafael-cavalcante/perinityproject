ffe9cdb989b2998a58cd6443b37e290d78a8628d
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2024-05-31 16:00:27 -0300

    Create DepartamentoServiceImplement.java

---
+++ src/main/java/com/rafaelcavalcante/perinityproject/service/implement/DepartamentoServiceImplement.java
@@ -0,0 +1,55 @@
+package com.rafaelcavalcante.perinityproject.service.implement;
+
+import com.rafaelcavalcante.perinityproject.config.exception.ResourceNotFoundException;
+import com.rafaelcavalcante.perinityproject.model.Departamento;
+import com.rafaelcavalcante.perinityproject.model.dto.DepartamentoDTO;
+import com.rafaelcavalcante.perinityproject.repository.DepartamentoRepository;
+import com.rafaelcavalcante.perinityproject.service.DepartamentoService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DepartamentoServiceImplement implements DepartamentoService {
+    private DepartamentoRepository departamentoRepository;
+
+    public DepartamentoServiceImplement(DepartamentoRepository departamentoRepository) {
+        this.departamentoRepository = departamentoRepository;
+    }
+
+    @Override
+    public Departamento adicionarDepartamento(Departamento departamento) {
+        return this.departamentoRepository.save(departamento);
+    }
+
+    @Override
+    public List<DepartamentoDTO> listarDepartamentos() {
+        return this.departamentoRepository.findDepartamentosStats();
+    }
+
+    @Override
+    public Departamento atualizarDepartamento(Long id, Departamento departamento) {
+        Optional<Departamento> optionalDepartamento = this.departamentoRepository.findById(id);
+
+        if (optionalDepartamento.isPresent()) {
+            Departamento departamentoExistente = optionalDepartamento.get();
+            departamentoExistente.setNome(departamento.getNome());
+
+            return this.departamentoRepository.save(departamentoExistente);
+        } else {
+            throw new ResourceNotFoundException("Departamento não encontrada com id " + id);
+        }
+    }
+
+    @Override
+    public void deletarDepartamento(Long id) {
+        Optional<Departamento> optionalDepartamento = this.departamentoRepository.findById(id);
+
+        if (optionalDepartamento.isPresent()) {
+            this.departamentoRepository.deleteById(id);
+        } else {
+            throw new ResourceNotFoundException("Departamento não encontrada com id " + id);
+        }
+    }
+}
