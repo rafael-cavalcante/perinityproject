0752ea1cf91ac546f79e1cd01cdd71f948efc45c
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2024-05-31 15:59:37 -0300

    Create GlobalExceptionHandle.java

---
+++ src/main/java/com/rafaelcavalcante/perinityproject/config/exception/GlobalExceptionHandle.java
@@ -0,0 +1,25 @@
+package com.rafaelcavalcante.perinityproject.config.exception;
+
+import com.rafaelcavalcante.perinityproject.config.exception.error.ErrorDetails;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandle {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException ex, HttpServletRequest request) {
+        ErrorDetails error = new ErrorDetails(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(ResourceNotEqualException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotEqualException(ResourceNotEqualException ex, HttpServletRequest request) {
+        ErrorDetails error = new ErrorDetails(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+}
