a751844ffe7d0591d8e186eaac324006ae179211
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2024-05-31 14:44:52 -0300

    Projeto Base

---
+++ src/main/java/com/rafaelcavalcante/perinityproject/PerinityprojectApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelcavalcante.perinityproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerinityprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerinityprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelcavalcante/perinityproject/PerinityprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelcavalcante.perinityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerinityprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
