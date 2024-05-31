952b7d7b093abcbbb556b2c02a1aa0714df3a4ae
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2024-05-31 16:04:35 -0300

    Update DepartamentoServiceImplement.java

--- src/main/java/com/rafaelcavalcante/perinityproject/service/implement/DepartamentoServiceImplement.java
+++ src/main/java/com/rafaelcavalcante/perinityproject/service/implement/DepartamentoServiceImplement.java
@@ -5,12 +5,14 @@ import com.rafaelcavalcante.perinityproject.model.Departamento;
 import com.rafaelcavalcante.perinityproject.model.dto.DepartamentoDTO;
 import com.rafaelcavalcante.perinityproject.repository.DepartamentoRepository;
 import com.rafaelcavalcante.perinityproject.service.DepartamentoService;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class DepartamentoServiceImplement implements DepartamentoService {
     private DepartamentoRepository departamentoRepository;
 
